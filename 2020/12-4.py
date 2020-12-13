9d42b11d93b67f27fb86b708907bc797ac5d6091
TheSlowHipster <garnetdroppo@gmail.com>
2020-12-12 22:52:56 -0800

    Finished with day 4

--- 2020/12-4.py
+++ 2020/12-4.py
@@ -12,7 +12,6 @@ def reader():
     l += 1
     while(l < len(f)):
         if(f[l] == ''):
-            print(line)
             _file.append(line)
             l += 1
             line = f[l].split(" ")
@@ -20,23 +19,48 @@ def reader():
             for el in f[l].split(" "):
                 line.append(el)
         l += 1
-    print(line)
     _file.append(line)
     return _file
 
 def isValid(line):
     ret = 7
     fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+    eyecol = ['amb','blu','brn','gry','grn','hzl','oth']
     for element in line:
-        tmp = list(filter(element.startswith, fields))
-        if (tmp != []):
-            ret -= 1 
+        element = element.split(":")
+        tmp = list(filter(element[0].startswith, fields))
+        if tmp != []:
+            if tmp[0] == 'byr':
+                if 1920 <= int(element[1]) <= 2002:
+                    ret -= 1
+            elif tmp[0] == 'iyr':
+                if 2010 <= int(element[1]) <= 2020:
+                    ret -= 1
+            elif tmp[0] == 'eyr':
+                if 2020 <= int(element[1]) <= 2030:
+                    ret -= 1
+            elif tmp[0] == 'hgt':
+                if element[1].endswith('in'):
+                    if 59 <= int(element[1][:-2]) <= 76:
+                        ret -= 1
+                elif element[1].endswith('cm'):
+                    if 150 <= int(element[1][:-2]) <= 193:
+                        ret -= 1
+            elif tmp[0] == 'hcl':
+
+                if len(element[1][0:]) == 7 and element[1][0] == '#':
+                    ret -= 1
+            elif tmp[0] == 'ecl':
+                alt = list(filter(element[1].startswith, eyecol))
+                if alt != [] and len(alt[0]) == 3:
+                    ret -= 1
+            elif tmp[0] == 'pid':
+                if element[1].isdigit() and len(element[1]) == 9:
+                    ret -= 1
     if ret > 0:
         ret = 0
     else:
         ret = 1
     return ret 
-
-    
 
 print(problem1())
