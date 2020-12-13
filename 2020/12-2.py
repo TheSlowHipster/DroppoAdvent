012539b79f623fd1b34f95db2e7c6f7b2480e958
TheSlowHipster <garnetdroppo@gmail.com>
2020-12-12 19:38:44 -0800

    Finished day 2

--- 2020/12-2.py
+++ 2020/12-2.py
@@ -12,10 +12,26 @@ def part1():
         print(f'Count: {count}')
         print(f'Password: {line[2]}')
         if _min <= count <= _max:
-            ans += 0
-        else:
             ans += 1
         print()
     return ans
 
+def part2():
+    ans = 0
+    for l in open("12-2-input","r").read().splitlines():
+        line = l.split(" ")
+        tmp = line[0].split("-")
+        left = line[2][int(tmp[0])-1]
+        right = line[2][int(tmp[1])-1]
+        char = line[1][0]
+        print(f'Left, Right: ({left}, {right})')
+        print(f'Character: {char}')
+        print(f'Password: {line[2]}')
+        print()
+        if left == char or right == char:
+            if left != right:
+                ans += 1
+    return ans
+
 print(part1())
+print(part2())
